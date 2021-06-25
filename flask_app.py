e0784261499febef3aa03f09a1ff7ba51492aca6
ArturTomczak <artomczak@gmail.com>
2021-06-25 17:26:05 +0200

    init flask index

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,18 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask
+from markupsafe import escape
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from IDS-web-app! -Artur'
+    return render_template('index.html')
+
+@app.route('/<myname>')
+def hello_you(myname):
+    return f'<h1>Hello {escape(myname)}</h1>'
+
+
+if __name__ == '__main__':
+    app.run()
 
