27f14de21ce41e2457e105c6607ed1f123b74aed
ArturTomczak <artomczak@gmail.com>
2021-07-21 16:38:57 +0200

    update interface

--- flask_app.py
+++ flask_app.py
@@ -7,156 +7,182 @@ from flask import session
 from flask import url_for
 import copy
 from werkzeug.exceptions import RequestedRangeNotSatisfiable
+from secret import SECRET_KEY
 
 app = Flask(__name__)
-app.secret_key = '16g2evbdc8723nd9812s9x'
+app.secret_key = SECRET_KEY
 
-@app.route('/')
+@app.route("/")
 def home_page():
-    return render_template('home.html')
+    return render_template("home.html")
 
-@app.route('/create', methods=['POST','GET'])
+
+@app.route("/create/", methods=["POST", "GET"])
 def create_page():
-    if request.method == 'POST':
+    if request.method == "POST":
         # TODO implement form actions
         # ids_db.append({'id': request.form['id'], 'content': request.form['content']})
         # session.modified = True
-        return redirect('/create')
+        return redirect("/create")
     else:
-        if 'ids_db' not in session:
-            session['ids_db'] = DEFAULT_IDS
+        if "ids_db" not in session:
+            session["ids_db"] = DEFAULT_IDS
         session.modified = True
-        return render_template('create.html', ids=session['ids_db'])
+        # TODO prevent accordion collapse on refresh
+        # if 'ids_ui' not in session:
+        #     session['ids_ui'] = {'accordions': ([0]*len(session['ids_db']))}
+        #     session['ids_ui']['accordions'][0] = 1
+        # session.modified = True
+        return render_template("create.html", ids=session["ids_db"])
 
-@app.route('/delete/<int:id>')
+
+@app.route("/delete/<int:id>")
 def delete(id):
     try:
-        for i in range(len(session['ids_db'])):
-            session['ids_db'] = list(filter(lambda i: i['id'] != id, session['ids_db']))
-        #renumber:
-        for i in range(len(session['ids_db'])):
-            session['ids_db'][i]['id'] = i+1
+        for i in range(len(session["ids_db"])):
+            session["ids_db"] = list(filter(lambda i: i["id"] != id, session["ids_db"]))
+        # renumber:
+        for i in range(len(session["ids_db"])):
+            session["ids_db"][i]["id"] = i + 1
         session.modified = True
-        return redirect('/create')
+        return redirect("/create")
     except:
-        return 'There was an error while deleting that specification.'
+        return "There was an error while deleting that specification."
 
-@app.route('/duplicate/<int:id>')
+
+@app.route("/duplicate/<int:id>")
 def duplicate(id):
     try:
-        for i in range(len(session['ids_db'])):
-            if session['ids_db'][i]['id'] == id:
-                new = copy.deepcopy(session['ids_db'][i])
-                new['id'] = id+1
-                session['ids_db'].insert(id, new)
+        for i in range(len(session["ids_db"])):
+            if session["ids_db"][i]["id"] == id:
+                new = copy.deepcopy(session["ids_db"][i])
+                new["id"] = id + 1
+                session["ids_db"].insert(id, new)
                 session.modified = True
-        #renumber:
-        for j in range(len(session['ids_db'])):
-            session['ids_db'][j]['id'] = j+1
+        # renumber:
+        for j in range(len(session["ids_db"])):
+            session["ids_db"][j]["id"] = j + 1
             session.modified = True
-        return redirect('/create')
+        return redirect("/create")
     except:
-        return 'There was an error while duplicating that specification.'
+        return "There was an error while duplicating that specification."
 
-@app.route('/add_specification')
+
+@app.route("/add_specification")
 def add_specification():
-    for i in range(len(session['ids_db'])):
-        session['ids_db'][i]['id'] = i+1
-    session['ids_db'].append(
-        {'id':len(session['ids_db'])+1, 'name':'New specification', 
-        'applicability':[
-            {'id': 1, 'type':'select', 'pre':'All', 'val':'IfcElements...'}
+    for i in range(len(session["ids_db"])):
+        session["ids_db"][i]["id"] = i + 1
+    session["ids_db"].append(
+        {
+            "id": len(session["ids_db"]) + 1,
+            "name": "New specification",
+            "applicability": [{"id": 1, "type": "entity", "value": ["IfcElement"]}],
+            "requirements": [
+                # {'id': 1.1, 'type':'property', 'pset':'Thermal', 'property':'U-value', 'value':'0.30'},
+                # {'id': 1.2, 'type':'property', 'pset':'Thermal', 'property':'U-value', 'value':'0.30'},
+                # {'id': 1.3, 'type':'property', 'pset':'Thermal', 'property':'U-value', 'value':'0.30'},
             ],
-        'requirements':[
-            # {'id': 1.1, 'type':'text', 'pre':'ZAbc', 'val':'ZDef'}, 
-            # {'id': 1.2, 'type':'text', 'pre':'ZAbc2', 'val':'ZDe2f'}, 
-            # {'id': 1.3, 'type':'text', 'pre':'ZAbc3', 'val':'ZDef3'}
-            ]
-        })
+        }
+    )
     session.modified = True
-    return redirect('/create')
- 
-@app.route('/add_requirement/<id>')
+    return redirect("/create")
+
+
+@app.route("/add_requirement/<id>")
 def add_requirement(id):
-    id, ar, fac = id.split('.')
-    id = int(id)-1
-    if fac == 'material':
-        type = 'text'
-        pre = 'material'
-        val = '<your_material>'
-    elif fac == 'property':
-        type = 'text'
-        pre = 'property'
-        val = '<your_property>'
-    elif fac == 'entity':
-        type = 'select'
-        pre = 'entity Ifc'
-        val = '<your_entity>'
-    elif fac == 'predefined':
-        type = 'text'
-        pre = 'predefined type'
-        val = '<your_predefined_type>'
-    elif fac == 'classification':
-        type = 'text'
-        pre = 'classification'
-        val = '<your_classification>'
-
-    if ar == 'applicability':
-        pre = 'of '+pre
-    if ar == 'requirements':
-        if fac == 'entity':
-            pre = 'an '+pre
-        else:
-            pre = 'a '+pre
-
-    session['ids_db'][id][ar].append({'id': len(session['ids_db'][id][ar])+1, 'type':type, 'pre':pre, 'val':val})
+    sid, part, type = id.split(".")
+    sid = int(sid) - 1
+    val = "" #my %s..." % type
+    if type == 'entity':
+        session["ids_db"][sid]['applicability'][0]['value'].append(val) 
+    else:
+        req = {"id": len(session["ids_db"][sid][part]) + 1, "type": type, "value": val}
+        if type == "property":
+            req["pset"] = ""
+            req["property"] = ""
+        elif type == "classification":
+            req["system"] = ""
+        session["ids_db"][sid][part].append(req)
     session.modified = True
-    return redirect('/create')
+    return redirect("/create")
 
-@app.route('/delete_requirement/<id>')
+
+@app.route("/delete_requirement/<id>")
 def delete_requirement(id):
-    id, rid, ar = id.split('.')
-    id = int(id)-1
+    sid, rid, part, type = id.split(".")
+    sid = int(sid) - 1
     rid = int(rid)
     try:
-        for i in range(len(session['ids_db'][id][ar])):
-            session['ids_db'][id][ar] = list(filter(lambda i: i['id'] != rid, session['ids_db'][id][ar]))
-        #renumber:
-        for i in range(len(session['ids_db'][id][ar])):
-            session['ids_db'][id][ar][i]['id'] = i+1
+        if type == "entity":
+            if len(session["ids_db"][sid]['applicability'][0]['value']) > 1:
+                del session["ids_db"][sid]['applicability'][0]['value'][-1]
+            else:
+                #TODO should inform user with a toast: 'can't delete the last entity'
+                pass
+        else:
+            for i in range(len(session["ids_db"][sid][part])):
+                session["ids_db"][sid][part] = list(filter(lambda i: i["id"] != rid, session["ids_db"][sid][part]))
+            # renumber:
+            for i in range(len(session["ids_db"][sid][part])):
+                session["ids_db"][sid][part][i]["id"] = i + 1
         session.modified = True
-        return redirect('/create')
+        return redirect("/create")
     except:
-        return 'There was an error while deleting that requirement'
+        return "There was an error while deleting that requirement"
 
-@app.route('/validate')
+
+@app.route("/validate")
 def validate_page():
-    if 'ids_db' not in session:
-        session['ids_db'] = DEFAULT_IDS
+    if "ids_db" not in session:
+        session["ids_db"] = DEFAULT_IDS
         session.modified = True
-    return render_template('validate.html', ids=session['ids_db'])
-    
-DEFAULT_IDS = [
-    {'id':1, 'name':'Fire requirement specification', 
-    'applicability':[
-        {'id': 1, 'type':'select', 'pre':'All', 'val':'Walls'}, 
-        {'id': 2, 'type':'text', 'pre':'of predefined type', 'val':'CLADDING'}, 
-        ],
-    'requirements':[
-        {'id': 1, 'type':'text', 'pre':'a material', 'val':'TIMBER'}, 
-        {'id': 2, 'type':'text', 'pre':'a property', 'val':'Class C30'},
-        ]
-    },
-    {'id':2, 'name':'Material specification', 
-    'applicability':[
-        {'id': 1, 'type':'select', 'pre':'All', 'val':'Windows'}, 
-        {'id': 2, 'type':'text', 'pre':'of material', 'val':'timber'}, 
-        ],
-    'requirements':[
-        {'id': 1, 'type':'text', 'pre':'a property', 'val':'U-value'}, 
-        ]
-    }
-    ]
+    return render_template("validate.html", ids=session["ids_db"])
 
-if __name__ == '__main__':
+
+@app.route("/documentation")
+def documentation_page():
+    if "ids_db" not in session:
+        session["ids_db"] = DEFAULT_IDS
+        session.modified = True
+    return render_template("documentation.html", ids=session["ids_db"])
+
+DEFAULT_IDS = [
+    {
+        "id": 1,
+        "name": "Fire requirement specification",
+        "applicability": [
+            {"id": 1, "type": "entity", "value": ["IfcWall"]},
+            {"id": 2, "type": "predefined", "value": "CLADDING"},
+        ],
+        "requirements": [
+            {"id": 1, "type": "material", "value": "TIMBER"},
+            {
+                "id": 2,
+                "type": "property",
+                "pset": "Structural",
+                "property": "Strength class",
+                "value": "C30",
+            },
+        ],
+    },
+    {
+        "id": 2,
+        "name": "Test specification",
+        "applicability": [
+            {"id": 1, "type": "entity", "value": ["IfcWall"]},
+            {"id": 2, "type": "predefined", "value": "TEST"},
+            {"id": 3, "type": "material", "value": "TEST"},
+            {"id": 4, "type": "classification", "value": "TEST", "system":"TEST"},
+            {"id": 5, "type": "property", "value": "TEST", "pset":"TEST", "property":"TEST"}
+        ],
+        "requirements": [
+            {"id": 1, "type": "predefined", "value": "TEST"},
+            {"id": 2, "type": "material", "value": "TEST"},
+            {"id": 3, "type": "classification", "value": "TEST", "system":"TEST"},
+            {"id": 4, "type": "property", "value": "TEST", "pset":"TEST", "property":"TEST"}
+        ],
+    },
+]
+
+if __name__ == "__main__":
     app.run()
