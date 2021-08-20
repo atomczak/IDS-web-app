6e16a18af972518035aeccecd50a78e861081129
ArturTomczak <artomczak@gmail.com>
2021-08-20 12:48:34 +0200

    add libraries

---
+++ python-libs/ifcopenshell/mvd/__init__.py
@@ -0,0 +1,184 @@
+from  . import mvdxml_expression
+
+from xml.dom.minidom import parse, Element
+
+class rule(object):
+    """
+    A class for representing an mvdXML EntityRule or AttributeRule
+    """
+
+    def __init__(self, tag, attribute, nodes, bind=None, optional=False):
+        self.tag, self.attribute, self.nodes, self.bind = tag, attribute, nodes, bind
+        self.optional = optional
+
+    def to_string(self, indent=0):
+        # return "%s%s%s[%s](%s%s)%s" % ("\n" if indent else "", " "*indent, self.tag, self.attribute, "".join(n.to_string(indent+2) for n in self.nodes), ("\n" + " "*indent) if len(self.nodes) else "", (" -> %s" % self.bind) if self.bind else "")
+        return "<Rule %s %s>" % (self.tag, self.attribute)
+
+    def __repr__(self):
+        return self.to_string()
+    
+class template(object):
+    """
+    Representation of an mvdXML template
+    """
+
+    def __init__(self, concept, root, constraints=None, rules=None):
+        self.concept, self.root, self.constraints = concept, root, (constraints or [])
+        self.rules = rules or []
+        self.entity = str(root.attributes['applicableEntity'].value)
+        try:
+            self.name = root.attributes['name'].value
+        except:
+            self.name = None
+        
+    def bind(self, constraints):
+        return template(self.concept, self.root, constraints, self.rules)
+        
+    def parse(self):
+        for rules in self.root.getElementsByTagName("Rules"):
+            for r in rules.childNodes:
+                if not isinstance(r, Element): continue
+                self.rules.append(self.parse_rule(r))
+
+                
+    def traverse(self, fn, root=None, with_parents=False):
+        def visit(n, p=root, ps=[root]):
+            if with_parents:
+                close = fn(rule=n, parents=ps)
+            else:
+                close = fn(rule=n, parent=p)
+
+            for s in n.nodes:
+                visit(s, n, ps + [n])
+
+            if close:
+                close()
+
+        for r in self.rules:
+            visit(r)
+                
+    def parse_rule(self, root):
+        def visit(node, prefix=""):
+            r = None
+            n = node
+            nm = None
+            p = prefix
+            optional=False
+            
+            if node.tagName == "AttributeRule":
+                r = node.attributes["AttributeName"].value
+                try:
+                    nm = node.attributes["RuleID"].value
+                except:
+                    # without binding, it's wrapped in a SPARQL OPTIONAL {} clause
+                    # Aim is to insert this clause once as high in the stack as possible
+                    # All topmost attribute rules are optional anyway as in the binding requirements on existence is specified
+
+                    def child_has_ruleid_or_prefix(node):
+                        if type(node).__name__ == "Element":
+                            if "RuleID" in node.attributes or "IdPrefix" in node.attributes:
+                                return True
+                            for n in node.childNodes:
+                                if child_has_ruleid_or_prefix(n): return True
+
+                    optional = node.parentNode.tagName == "Rules" or not child_has_ruleid_or_prefix(node)
+            elif node.tagName == "EntityRule":
+                r = node.attributes["EntityName"].value
+            elif node.tagName == "References":
+                ref = node.getElementsByTagName("Template")[0].attributes['ref'].value
+                n = self.concept.template(ref).root
+                try: p = p + node.attributes["IdPrefix"].value
+                except: pass
+            elif node.tagName == "Constraint":
+                r = mvdxml_expression.parse(node.attributes["Expression"].value)
+                
+            def _(n):
+                for subnode in n.childNodes:
+                    if not isinstance(subnode, Element): continue
+                    for x in visit(subnode, p): yield x
+
+            if r:
+                yield rule(node.tagName, r, list(_(n)), (p + nm) if nm else nm, optional=optional)
+            else:
+                for subnode in n.childNodes:
+                    if not isinstance(subnode, Element): continue
+                    for x in visit(subnode, p): yield x
+            
+        return list(visit(root))[0]
+
+class concept_or_applicability(object):
+    """
+    Representation of either a mvdXML Concept or the Applicability node. Basically a structure
+    for the hierarchical TemplateRule
+    """
+
+    def __init__(self, root, c):
+        self.root = root
+        self.concept_node = c
+        try:
+            self.name = c.attributes["name"].value
+        except:
+            # probably applicability and not concept
+            self.name = "Applicability"
+
+    def template(self, id = None):
+        if id is None:
+            id = self.concept_node.getElementsByTagName("Template")[0].attributes['ref'].value
+
+        for node in self.root.dom.getElementsByTagName("ConceptTemplate"):
+            if node.attributes["uuid"].value == id:
+                t = template(self, node)
+                t.parse()
+                t_with_rules = t.bind(self.rules())
+                return t_with_rules
+
+
+    def rules(self):
+        # Get the top most TemplateRule and traverse
+        try:
+            rules = self.concept_node.getElementsByTagName("TemplateRules")[0]
+        except:
+            return []
+
+        def visit(rules):
+            def _():
+                for i, r in enumerate([c for c in rules.childNodes if isinstance(c, Element)]):
+                    if i:
+                        yield rules.attributes["operator"].value
+                    if r.tagName == "TemplateRules":
+                        yield visit(r)
+                    elif r.tagName == "TemplateRule":
+                        yield mvdxml_expression.parse(r.attributes["Parameters"].value)
+                    else:
+                        raise Exception()
+
+            return list(_())
+
+        return visit(rules)
+    
+class concept_root(object):
+    def __init__(self, dom, root):
+        self.dom, self.root = dom, root
+        self.name = root.attributes['name'].value
+        self.entity = str(root.attributes['applicableRootEntity'].value)
+
+    def applicability(self):
+        return concept_or_applicability(self, self.root.getElementsByTagName("Applicability")[0])
+        
+    def concepts(self):
+        for c in self.root.getElementsByTagName("Concept"):
+            yield concept_or_applicability(self, c)
+
+    @staticmethod
+    def parse(fn):
+        dom = parse(fn)
+        if len(dom.getElementsByTagName("ConceptRoot")):
+            for root in dom.getElementsByTagName("ConceptRoot"):
+                CR = concept_root(dom, root)
+                yield CR
+        else:
+            for templ in dom.getElementsByTagName("ConceptTemplate"):
+                t = template(None, templ)
+                t.parse()
+                yield t
---
+++ python-libs/ifcopenshell/util/element.py
@@ -0,0 +1,136 @@
+import ifcopenshell
+
+
+def get_psets(element):
+    psets = {}
+    if element.is_a("IfcTypeObject"):
+        if element.HasPropertySets:
+            for definition in element.HasPropertySets:
+                psets[definition.Name] = get_property_definition(definition)
+    elif hasattr(element, "IsDefinedBy"):
+        for relationship in element.IsDefinedBy:
+            if relationship.is_a("IfcRelDefinesByProperties"):
+                definition = relationship.RelatingPropertyDefinition
+                psets[definition.Name] = get_property_definition(definition)
+    return psets
+
+
+def get_property_definition(definition):
+    if definition is not None:
+        props = {}
+        if definition.is_a("IfcElementQuantity"):
+            props.update(get_quantities(definition.Quantities))
+        elif definition.is_a("IfcPropertySet"):
+            props.update(get_properties(definition.HasProperties))
+        else:
+            # Entity introduced in IFC4
+            # definition.is_a('IfcPreDefinedPropertySet'):
+            for prop in range(4, len(definition)):
+                props[definition.attribute_name(prop)] = definition[prop]
+        return props
+
+
+def get_quantities(quantities):
+    results = {}
+    for quantity in quantities:
+        if quantity.is_a("IfcPhysicalSimpleQuantity"):
+            results[quantity.Name] = quantity[3]
+    return results
+
+
+def get_properties(properties):
+    results = {}
+    for prop in properties:
+        if prop.is_a("IfcPropertySingleValue"):
+            results[prop.Name] = prop.NominalValue.wrappedValue if prop.NominalValue else None
+        elif prop.is_a("IfcComplexProperty"):
+            data = prop.get_info()
+            data["properties"] = get_properties(prop.HasProperties)
+            del data["HasProperties"]
+            results[prop.Name] = data
+    return results
+
+
+def get_type(element):
+    if element.is_a("IfcTypeObject"):
+        return element
+    elif hasattr(element, "IsTypedBy") and element.IsTypedBy:
+        return element.IsTypedBy[0].RelatingType
+    elif hasattr(element, "IsDefinedBy") and element.IsDefinedBy:  # IFC2X3
+        for relationship in element.IsDefinedBy:
+            if relationship.is_a("IfcRelDefinesByType"):
+                return relationship.RelatingType
+
+
+def get_material(element, should_skip_usage=False):
+    if hasattr(element, "HasAssociations") and element.HasAssociations:
+        for relationship in element.HasAssociations:
+            if relationship.is_a("IfcRelAssociatesMaterial"):
+                if should_skip_usage:
+                    if relationship.RelatingMaterial.is_a("IfcMaterialLayerSetUsage"):
+                        return relationship.RelatingMaterial.ForLayerSet
+                    elif relationship.RelatingMaterial.is_a("IfcMaterialProfileSetUsage"):
+                        return relationship.RelatingMaterial.ForProfileSet
+                return relationship.RelatingMaterial
+    relating_type = get_type(element)
+    if hasattr(relating_type, "HasAssociations") and relating_type.HasAssociations:
+        for relationship in relating_type.HasAssociations:
+            if relationship.is_a("IfcRelAssociatesMaterial"):
+                return relationship.RelatingMaterial
+
+
+def get_container(element):
+    if hasattr(element, "ContainedInStructure") and element.ContainedInStructure:
+        return element.ContainedInStructure[0].RelatingStructure
+
+
+def get_aggregate(element):
+    if hasattr(element, "Decomposes") and element.Decomposes:
+        return element.Decomposes[0].RelatingObject
+
+
+def replace_attribute(element, old, new):
+    for i, attribute in enumerate(element):
+        if attribute == old:
+            element[i] = new
+        elif isinstance(attribute, tuple):
+            new_attribute = list(attribute)
+            for j, item in enumerate(attribute):
+                if item == old:
+                    new_attribute[j] = new
+                    element[i] = new_attribute
+
+
+def remove_deep(ifc_file, element):
+    # @todo maybe some sort of try-finally mechanism.
+    ifc_file.batch()
+    subgraph = list(ifc_file.traverse(element))
+    subgraph_set = set(subgraph)
+    for ref in subgraph[::-1]:
+        if ref.id() and len(set(ifc_file.get_inverse(ref)) - subgraph_set) == 0:
+            ifc_file.remove(ref)
+    ifc_file.unbatch()
+
+
+def copy(ifc_file, element):
+    new = ifc_file.create_entity(element.is_a())
+    for i, attribute in enumerate(element):
+        if attribute is None:
+            continue
+        new[i] = attribute
+    return new
+
+
+def copy_deep(ifc_file, element):
+    new = ifc_file.create_entity(element.is_a())
+    for i, attribute in enumerate(element):
+        if attribute is None:
+            continue
+        if isinstance(attribute, ifcopenshell.entity_instance):
+            attribute = copy_deep(ifc_file, attribute)
+        elif isinstance(attribute, tuple) and attribute and isinstance(attribute[0], ifcopenshell.entity_instance):
+            attribute = list(attribute)
+            for j, item in enumerate(attribute):
+                attribute[j] = copy_deep(ifc_file, item)
+        new[i] = attribute
+    return new
